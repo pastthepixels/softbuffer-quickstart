2d6028cda120cf4892085e528962e40ab030d0ba
PastThePixels <pastthescreen@protonmail.com>
2024-07-25 09:33:15 -0600

    Clear main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,1 @@
-fn main() {
-    /*
-    // Example:
-    let mut x = 0;
-    let mut window = SoftbufferWindow::new(|window| {
-        x += 1;
-        println!("{}", x);
-        vec![0xffffff; (window.width * window.height) as usize].into_boxed_slice()
-    });
-    window.run().expect("damn! :(");
-    */
-}
+fn main() {}
